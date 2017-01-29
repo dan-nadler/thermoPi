e2410a8ac82fcd3952636e89a8ea913913f550c0
Dan Nadler <dnadler87@gmail.com>
2017-01-29 18:55:12 -0500

    began work on dashboard page using Highcharts

--- thermo/monitor/app.py
+++ thermo/monitor/app.py
@@ -1,23 +1,13 @@
-from thermo.common.models import get_engine, Temperature, Sensor
-from thermo.analysis.IO import get_plotting_dataframe
+from thermo.analysis.IO import get_plotting_dataframe, get_dataframe
 from datetime import datetime, timedelta
-from sqlalchemy.orm import sessionmaker
-import pandas as pd
+import time
+import pytz
 from flask import Flask, render_template
-
-engine = get_engine()
-Session = sessionmaker(bind=engine)
-
-def get_data(lookback=3):
-    session = Session()
-    q = session.query(Temperature).filter(Temperature.record_time >= datetime.now() - timedelta(hours=lookback))
-    df = pd.read_sql(q.statement, q.session.bind)
-    session.close()
-    return df
+from numpy import isnan
 
 
 class Cache:
-    def __init__(self, cache_duration_seconds=5):
+    def __init__(self, cache_duration_seconds=60):
         """
         :param lookback:
         :param cache_duration_seconds: duration of cached in seconds
@@ -26,12 +16,11 @@ class Cache:
         self.last_retrieval = datetime.now() - timedelta(days=1000)
         self.df = None
 
-    def data(self):
+    def data(self, *args, **kwargs):
         cache_duration = self.cache_duration
         if (datetime.now() - self.last_retrieval) > timedelta(seconds=cache_duration):
-            print('updating cache')
             self.last_retrieval = datetime.now()
-            self.df = self._get_data()
+            self.df = self._get_data(*args, **kwargs)
         return self.df
 
     def _get_data(self):
@@ -43,11 +32,11 @@ class RawDataFrame(Cache):
         super(RawDataFrame, self).__init__(*args, **kwargs)
 
     def _get_data(self, lookback=3):
-        return get_data(lookback)
+        return get_dataframe(lookback)
 
 
 class PlotDataFrame(Cache):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, lookback=3, **kwargs):
         super(PlotDataFrame, self).__init__(*args, **kwargs)
 
     def _get_data(self, lookback=3):
@@ -60,7 +49,7 @@ class RecentTemperature(Cache):
 
     def _get_data(self):
         df = get_plotting_dataframe(hours=3, resolution='10S')
-        df = df.resample('60S').last()
+        df = df.dropna().resample('60S').last()
         return df
 
 
@@ -70,12 +59,16 @@ last_data = RecentTemperature(cache_duration_seconds=10)
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-def index(chartID = 'Current', chart_type = 'bar', chart_height = 350):
-    data1 = last_data.data().ix[-1, :]
-    data2 = last_data.data().ix[0, :]
-    chart = {"renderTo": chartID, "type": chart_type, "height": chart_height, }
+
+def current_temp_chart(chartID, chart_height, chart_type):
+    data1 = last_data.data().ix[-1, :].fillna(0)
+    data2 = last_data.data().ix[0, :].fillna(0)
+    chart = {
+        "renderTo": chartID,
+        "type": chart_type,
+        "height": chart_height,
+        "width": 650
+    }
     series = [
         {
             "name": str(data1.name),
@@ -106,22 +99,114 @@ def index(chartID = 'Current', chart_type = 'bar', chart_height = 350):
         },
     ]
     title = {"text": 'Current Temperature'}
-    xAxis = {"categories": list(data1.index)}
-    yAxis = {"title": {"text": 'Degrees Fahrenheit'}}
-    return render_template('index.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
+    xAxis = {
+        "categories": list(data1.index),
+        "labels": {
+            "enabled": "false"
+        }
+    }
+    yAxis = {
+        "title": {
+            "text": 'Degrees Fahrenheit'
+        },
+        "labels": {
+            "enabled": "false"
+        }
+
+    }
+    chart = {
+        'chartID': chartID,
+        'chart': chart,
+        'series': series,
+        'title': title,
+        'xAxis': xAxis,
+        'yAxis': yAxis,
+    }
+
+    return chart
+
+
+def temp_history_chart(chartID, chart_height):
+    data = chart_data.data(lookback=24).dropna()
+    localtz = pytz.timezone('America/New_York')
+    utctz = pytz.timezone('UTC')
+    chart = {
+        "renderTo": chartID,
+        "type": 'line',
+        "height": chart_height,
+        "width": 650,
+        "zoomType": 'x'
+    }
+
+    localize = lambda t: utctz.localize(t).astimezone(localtz).timetuple()
+
+    series = [
+        {
+            "name": str(d[1].name),
+            "yAxis": 1 if d[1].name == 'Outside (Street)' else 0,
+            # "type": 'line' if d[1].name == 'Outside (Street)' else 'line',
+            "zIndex": 0 if d[1].name == 'Outside (Street)' else 1,
+            "data": [[time.mktime(a) * 1000, b] for a, b in zip([localize(t) for t in d[1].index], d[1].values)],
+        } for d in data.iteritems()
+        ]
+    title = {"text": 'Historical Temperature'}
+    xAxis = {
+        "type": 'datetime',
+    }
+    yAxis = [
+        {
+            "title": {
+                "text": 'Degrees Fahrenheit'
+            }
+        },
+        {
+            "title": {
+                "text": 'Exterior Temperature'
+            },
+            "opposite": "true"
+        }
+    ]
+    chart = {
+        'chartID': chartID,
+        'chart': chart,
+        'series': series,
+        'title': title,
+        'xAxis': xAxis,
+        'yAxis': yAxis,
+    }
+    return chart
+
+
+@app.route('/')
+@app.route('/index')
+def index(chart_height=400):
+    charts = []
+
+    charts.append(
+        current_temp_chart('Current', chart_height, 'bar')
+    )
+
+    charts.append(
+        temp_history_chart('History', chart_height)
+    )
+
+    return render_template('index.html', charts=charts)
 
 
 @app.route('/raw')
 def raw():
     return data.data().to_html()
 
+
 @app.route('/chart-data/')
 def plot():
     return chart_data.data().to_html()
 
+
 @app.route('/last-data')
 def last():
     return last_data.data().to_frame().to_html()
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000, passthrough_errors=True)
