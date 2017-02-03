8887c6b280f9b712221212a526e173dddd3546e4
Dan Nadler <dnadler87@gmail.com>
2017-02-02 22:39:26 -0500

    add graceful exit from thermostat loop rename 'window' to 'minutes'

--- thermo/control/thermostat.py
+++ thermo/control/thermostat.py
@@ -46,13 +46,13 @@ class Thermostat():
     def __init__(self, user_id):
         self.user_id = user_id
 
-    def check_recent_temperature(self, window=5, verbose=False):
+    def check_recent_temperature(self, minutes=5, verbose=False):
         session = get_session()
         indoor_temperatures = session.query(
             Temperature.location,
             func.sum(Temperature.value) / func.count(Temperature.value)
         ).filter(
-            Temperature.record_time > datetime.now() - timedelta(minutes=window)
+            Temperature.record_time > datetime.now() - timedelta(minutes=minutes)
         ).join(Sensor).filter(
             Sensor.indoors == True and Sensor.user == self.user_id
         ).group_by(
@@ -70,21 +70,25 @@ if __name__ == '__main__':
 
     thermostat = Thermostat(1)
 
-    while True:
+    try:
+        while True:
+            room_temps = thermostat.check_recent_temperature(minutes=5)
 
-        room_temps = thermostat.check_recent_temperature(window=5)
+            target = 70
+            room = 'Living Room (South Wall)'
 
-        target = 70
-        room = 'Living Room (South Wall)'
+            print("%s: %.2f" % (datetime.now().strftime('%m/%d/%Y %H:%M:%S'), room_temps[room]))
 
-        print("%s: %.2f" % (datetime.now().strftime('%m/%d/%Y %H:%M:%S'), room_temps[room]))
+            if room_temps[room] < target and not zone1.heat_relay_is_on():
+                print("Turning heat on.")
+                zone1.turn_heat_on()
 
-        if room_temps[room] < target and not zone1.heat_relay_is_on():
-            print("Turning heat on.")
-            zone1.turn_heat_on()
+            elif room_temps[room] >= target and zone1.heat_relay_is_on():
+                print("Turning heat off.")
+                zone1.turn_heat_off()
 
-        elif room_temps[room] >= target and zone1.heat_relay_is_on():
-            print("Turning heat off.")
-            zone1.turn_heat_off()
+            time.sleep(10)
 
-        time.sleep(10)
+    except KeyboardInterrupt:
+        print("Turning heat off.")
+        zone1.turn_heat_off()
